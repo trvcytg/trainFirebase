31023726e6c67f52c171848b64c0c9e3d0530224
Todd Tracy <trvcytg@gmail.com>
2019-09-10 17:29:06 -0500

    Added firebase inititlaization template

---
+++ javascript.js
@@ -0,0 +1,9 @@
+const config = {
+  apiKey: " ***myFireBaseKEY here*** ",
+  authDomain: " ***myFireBase here*** .firebaseapp.com",
+  databaseURL: "https:// ***myFireBase here*** .firebaseio.com",
+  projectId: " ***myFireBase here*** ",
+  storageBucket: " ***myFireBase here*** .appspot.com"
+};
+
+firebase.initializeApp(config);

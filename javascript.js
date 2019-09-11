5f093a6d001494dd4ab4c6656b1faac7e2899b71
Todd Tracy <trvcytg@gmail.com>
2019-09-10 22:54:44 -0500

    Fixed Firebase reference

--- javascript.js
+++ javascript.js
@@ -1,12 +1,10 @@
 // Your web app's Firebase configuration
-var firebaseConfig = {
+const config = {
   apiKey: "AIzaSyClu67DVDZFdKt0MGp8Fd4TlZM8WR1-MHk",
   authDomain: "projectevergreen-e6084.firebaseapp.com",
   databaseURL: "https://projectevergreen-e6084.firebaseio.com",
   projectId: "projectevergreen-e6084",
-  storageBucket: "",
-  messagingSenderId: "711890008958",
-  appId: "1:711890008958:web:0e35109abf6e20eaa8912f"
+  storageBucket: "projectevergreen-e6084.appspot.com"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);

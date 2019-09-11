09ba98ee99e8c4f19493d4ce7e2dc56045e5d792
Todd Tracy <trvcytg@gmail.com>
2019-09-10 22:56:31 -0500

    Added code to store values on button press and print to html -- wordy, need to find more efficient code

--- javascript.js
+++ javascript.js
@@ -11,3 +11,50 @@ firebase.initializeApp(config);
 const database = firebase.database();
 
 $(`#submitButton`).click(function(event) {
+  event.preventDefault();
+  const trainName = $(`#trainName`)
+    .val()
+    .trim();
+  console.log(trainName);
+
+  const destination = $(`#destination`)
+    .val()
+    .trim();
+  console.log(destination);
+
+  const firstTrain = $(`#firstTrain`)
+    .val()
+    .trim();
+  console.log(firstTrain);
+
+  const trainFrequency = $(`#frequency`)
+    .val()
+    .trim();
+  console.log(trainFrequency);
+
+  database.ref().push({
+    name: trainName,
+    destination: destination,
+    firstTrain: firstTrain,
+    frequency: trainFrequency,
+    dateAdded: firebase.database.ServerValue.TIMESTAMP
+  });
+  $(`.trainPull`).empty();
+  database.ref().on("child_added", function(snapshot, prevChildKey) {
+    var newPost = snapshot.val();
+    console.log("Train Name: " + newPost.name);
+    console.log("Destination: " + newPost.destination);
+    console.log("First Train: " + newPost.firstTrain);
+    console.log("Frequency in minutes: " + newPost.frequency);
+    console.log("Previous Post ID: " + prevChildKey);
+    console.log(`-- - -- --- -- - --`);
+
+    newDiv = $(`<tr><th scope="row" class="nameData">${newPost.name}</th>
+        <td scope="row" class="trainDestination">${newPost.destination}</td>
+        <td class="trainFrequency">${newPost.frequency}</td>
+        <td class="nextTrain">change to NextArrivalTime</td>
+        <td class="minAway">change to MinutesUntilNextTrain</td></tr>`);
+    $(`.trainPull`).prepend(newDiv);
+  });
+});
+

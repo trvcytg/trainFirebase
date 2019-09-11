14ad5823709d9b2627f1e0e46419612bc6f889fd
Todd Tracy <trvcytg@gmail.com>
2019-09-11 15:28:54 -0500

    Added an initial check to see if there is information from Firebase to push to table upon loading of page

--- javascript.js
+++ javascript.js
@@ -10,6 +10,29 @@ const config = {
 firebase.initializeApp(config);
 const database = firebase.database();
 
+database.ref().once("value", function(snapshot) {
+  if (snapshot.exists()) {
+    $(`.trainPull`).empty();
+    database.ref().on("child_added", function(snapshot, prevChildKey) {
+      var newPost = snapshot.val();
+      console.log("***Initial Data Pulled from Firebase***");
+      console.log("Train Name: " + newPost.name);
+      console.log("Destination: " + newPost.destination);
+      console.log("First Train: " + newPost.firstTrain);
+      console.log("Frequency in minutes: " + newPost.frequency);
+      console.log("Previous Post ID: " + prevChildKey);
+      console.log(`-- - -- --- -- - --`);
+
+      newDiv = $(`<tr><th scope="row" class="nameData">${newPost.name}</th>
+      <td scope="row" class="trainDestination">${newPost.destination}</td>
+      <td class="trainFrequency">${newPost.frequency}</td>
+      <td class="nextTrain">change to NextArrivalTime</td>
+      <td class="minAway">change to MinutesUntilNextTrain</td></tr>`);
+      $(`.trainPull`).prepend(newDiv);
+    });
+  }
+});
+
 $(`#submitButton`).click(function(event) {
   event.preventDefault();
   const trainName = $(`#trainName`)

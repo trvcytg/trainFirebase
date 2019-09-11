060fe68e2de7a28be6120b79984505bc3033ee00
Todd Tracy <trvcytg@gmail.com>
2019-09-10 21:29:56 -0500

    updated Firebase links with accurate information

--- javascript.js
+++ javascript.js
@@ -1,9 +1,12 @@
-const config = {
-  apiKey: " ***myFireBaseKEY here*** ",
-  authDomain: " ***myFireBase here*** .firebaseapp.com",
-  databaseURL: "https:// ***myFireBase here*** .firebaseio.com",
-  projectId: " ***myFireBase here*** ",
-  storageBucket: " ***myFireBase here*** .appspot.com"
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyClu67DVDZFdKt0MGp8Fd4TlZM8WR1-MHk",
+  authDomain: "projectevergreen-e6084.firebaseapp.com",
+  databaseURL: "https://projectevergreen-e6084.firebaseio.com",
+  projectId: "projectevergreen-e6084",
+  storageBucket: "",
+  messagingSenderId: "711890008958",
+  appId: "1:711890008958:web:0e35109abf6e20eaa8912f"
 };
-
-firebase.initializeApp(config);
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
